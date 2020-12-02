f54372e171e009e4a875e0f752f34d64b80ade4a
MichaelKND <61099893+MichaelKND@users.noreply.github.com>
2020-12-02 02:20:24 -0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from datetime import datetime
 import operator
 import webbrowser
 import praw
@@ -19,9 +20,10 @@ class PostClassPRAW:
         self.score = rawPost.score
         self.ratio = rawPost.upvote_ratio
         self.comments = rawPost.num_comments
-        self.date = rawPost.created_utc
+        self.date = datetime.fromtimestamp(rawPost.created_utc)
         self.edited = rawPost.edited
         self.url = rawPost.url
+        self.content = rawPost.selftext
 
 
 POSTS_DATA = []
@@ -59,6 +61,7 @@ def replaceList(listbox, popList) -> NONE:
 
 
 def loadDataPRAW(listbox, sub, category, limit=20):
+    POSTS_DATA.clear()
     listbox.delete(0, END)
     returnedList = returnListFromSub(sub, category, int(limit))
     for num, post in enumerate(returnedList):
@@ -75,6 +78,7 @@ def show_gui() -> NONE:
     root_window.geometry('800x350')
 
     CATEGORY = StringVar()
+    COUNT = IntVar()
 
     "Creating the Frame that contains the Listbox of the display."
     postListFrame = Canvas(root_window)
@@ -90,6 +94,10 @@ def show_gui() -> NONE:
                           font=("Garamond", 12))
 
     postListScrollbar.config(command=postListbox.yview)
+    postListbox.config(yscrollcommand=postListScrollbar.set)
+
+    """Binding double click of the listbox to a function."""
+    postListbox.bind('<Double-1>', loadPostInfo)
 
     postListbox.bind("<B1-Leave>", lambda event: "break")
 
@@ -105,21 +113,27 @@ def show_gui() -> NONE:
     categories = {'hot', 'top', 'new', 'rising'}
     CATEGORY.set('hot')
     dropdownMenu = OptionMenu(displaySubreddit, CATEGORY, *categories)
-    dropdownMenu["highlightthickness"]=0
+    dropdownMenu["highlightthickness"] = 0
     dropdownMenu.grid(row=0, column=2, sticky='NW', padx=5, pady=8)
 
     postListbox.pack(expand=True, fill=Y)
 
-    """Creating the entry box to set the number of posts."""
-    entry_count = Entry(displaySubreddit, width=10)
-    entry_count.grid(row=0, column=3)
+    """Creating the dropdown menu to set the number of posts."""
+    # entry_count = Entry(displaySubreddit, width=10)
+    # entry_count.grid(row=0, column=3)
+
+    counts = {5, 10, 25, 50}
+    COUNT.set(5)
+    dropdownMenu = OptionMenu(displaySubreddit, COUNT, *sorted(counts))
+    dropdownMenu["highlightthickness"] = 0
+    dropdownMenu.grid(row=0, column=3, sticky='NW', padx=5, pady=8)
 
     """Creating the load button to load the posts."""
     button_Posts = Button(displaySubreddit, text="Reload Posts",
                           command=lambda: loadDataPRAW(postListbox,
                                                        entry_subreddit.get(),
                                                        CATEGORY.get(),
-                                                       entry_count.get()))
+                                                       COUNT.get()))
     button_Posts.grid(row=0, column=0, sticky=W, pady=5, padx=5)
 
     """Creating the Frame for the sort/filter buttons."""
@@ -195,6 +209,7 @@ def searchForAuthor(entryBox, listbox) -> NONE:
         if titleQuery in postListing.author:
             resultsList.append(postListing)
     replaceList(listbox, resultsList)
+    print(len(POSTS_DATA))
 
 
 def sortByTitle(listbox) -> NONE:
@@ -237,6 +252,104 @@ def filterForUnedited(listbox) -> NONE:
         if 'False' == postListing.edited:
             listOfResults.append(postListing)
     replaceList(listbox, listOfResults)
+
+
+def loadPostInfo(event):
+    listbox = event.widget
+    selectionIndex = listbox.curselection()[0]
+    selection = listbox.get(selectionIndex)
+    for postListing in POSTS_DATA:
+        if selection in postListing.title:
+            postPopup(postListing)
+
+
+def postPopup(post):
+    popUp = Tk()
+    popUp.wm_title(post.title)
+    popUp.config(background='#24A0ED')
+    popUp.geometry('680x700')
+
+    """Creating the content Frame."""
+    contentWindow = Frame(popUp, width=400, bg="#24A0ED")
+    contentWindow.grid(row=1, column=0, padx=17, columnspan=1)
+
+    label_popUp_content = Label(popUp, text="Post Text: ", bg='#24A0ED', fg='white')
+    label_popUp_content.grid(row=0, column=0)
+
+    "Creating the content window."
+    content = post.content
+    text_popUp_content = Text(contentWindow, height=20, width=100, font=("Arial Narrow", 10))
+    text_popUp_content.insert(END, content)
+    text_popUp_content.pack(side=LEFT)
+
+    """Creating a scrollbar."""
+    postListScrollbar = Scrollbar(contentWindow, orient="vertical")
+    postListScrollbar.pack(side=RIGHT, fill=Y)
+    postListScrollbar.config(command=text_popUp_content.yview)
+    text_popUp_content.bind("<B1-Leave>", lambda event: "break")
+    text_popUp_content.config(yscrollcommand=postListScrollbar.set)
+
+    """Creating the Frame for the single Post info display."""
+    displayPostInfo = Frame(popUp, width=400, bg="grey30")
+    displayPostInfo.grid(row=2, column=0, sticky='NW', padx=17, pady=10)
+
+    """Creating the post info displays."""
+    label_postTitle = Label(displayPostInfo,
+                            text="Title", bg="grey30", fg="white")
+    label_postTitle.grid(row=0, column=0, sticky=W, padx=10)
+    label_postAuthor = Label(displayPostInfo,
+                             text="Author", bg="grey30", fg="white")
+    label_postAuthor.grid(row=1, column=0, sticky=W, padx=10)
+    label_postScore = Label(displayPostInfo,
+                            text="Score", bg="grey30", fg="white")
+    label_postScore.grid(row=2, column=0, sticky=W, padx=10)
+    label_postRatio = Label(displayPostInfo,
+                            text="Upvote Ratio", bg="grey30", fg="white")
+    label_postRatio.grid(row=3, column=0, sticky=W, padx=10)
+    label_postComments = Label(displayPostInfo,
+                               text="Comments", bg="grey30", fg="white")
+    label_postComments.grid(row=4, column=0, sticky=W, padx=10)
+    label_postDate = Label(displayPostInfo,
+                           text="Date", bg="grey30", fg="white")
+    label_postDate.grid(row=5, column=0, sticky=W, padx=10)
+    label_postEdited = Label(displayPostInfo,
+                             text="Edited", bg="grey30", fg="white")
+    label_postEdited.grid(row=6, column=0, sticky=W, padx=10)
+    label_postUrl = Label(displayPostInfo,
+                          text="URL", bg="grey30", fg="white")
+    label_postUrl.grid(row=7, column=0, sticky=W, padx=10)
+
+    labelBox_postTitle = Label(displayPostInfo, text=post.title,
+                               anchor='w', width=78, bg="white")
+    labelBox_postTitle.grid(row=0, column=1, sticky=W)
+    labelBox_postAuthor = Label(displayPostInfo, text=post.author,
+                                anchor='w', width=78, bg="white")
+    labelBox_postAuthor.grid(row=1, column=1, sticky=W)
+    labelBox_postScore = Label(displayPostInfo, text=post.score,
+                               anchor='w', width=78, bg="white")
+    labelBox_postScore.grid(row=2, column=1, sticky=W)
+    labelBox_postRatio = Label(displayPostInfo, text=post.ratio,
+                               anchor='w', width=78, bg="white")
+    labelBox_postRatio.grid(row=3, column=1, sticky=W)
+    labelBox_postComments = Label(displayPostInfo, text=post.comments,
+                                  anchor='w', width=78, bg="white")
+    labelBox_postComments.grid(row=4, column=1, sticky=W)
+    labelBox_postDate = Label(displayPostInfo, text=post.date,
+                              anchor='w', width=78, bg="white")
+    labelBox_postDate.grid(row=5, column=1, sticky=W)
+    labelBox_postEdited = Label(displayPostInfo, text=post.edited,
+                                anchor='w', width=78, bg="white")
+    labelBox_postEdited.grid(row=6, column=1, sticky=W)
+    labelBox_postUrl = Label(displayPostInfo, text=post.url,
+                             anchor='w', width=78, bg="white")
+    labelBox_postUrl.grid(row=7, column=1, sticky=W)
+
+    """Creating the url button."""
+    button_URL = Button(popUp, text="Go to post", width=50,
+                        command=lambda: webbrowser.open(post.url))
+    button_URL.grid(row=3, column=0, pady=5, padx=100)
+
+    popUp.mainloop()
 
 
 if __name__ == '__main__':
